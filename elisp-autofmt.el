aafde992249b176aadddfa5897f1c20c8a602f95
Campbell Barton <ideasman42@gmail.com>
2022-05-22 15:16:23 +1000

    Initial introspection cache & tests

--- elisp-autofmt.el
+++ elisp-autofmt.el
@@ -7,7 +7,7 @@
 
 ;; URL: https://codeberg.org/ideasman42/emacs-elisp-autofmt
 ;; Version: 0.1
-;; Package-Requires: ((emacs "26.2"))
+;; Package-Requires: ((emacs "28.1"))
 
 ;;; Commentary:
 
@@ -26,12 +26,12 @@
 
 (defcustom elisp-autofmt-empty-line-max 2 "The maximum number of blank lines to keep." :type 'int)
 
-(defcustom elisp-autofmt-on-save-p 'elisp-autopep8-check-elisp-autofmt-exists
+(defcustom elisp-autofmt-on-save-p 'elisp-autofmt-check-elisp-autofmt-exists
   "Only reformat on save if this function returns non-nil.
 
 You may wish to choose one of the following options:
 - `always': To always format on save.
-- `elisp-autopep8-check-elisp-autofmt-exists':
+- `elisp-autofmt-check-elisp-autofmt-exists':
   Only reformat when \"pyproject.toml\" exists.
 
 Otherwise you can set this to a user defined function."
@@ -48,58 +48,300 @@ Can be slow!"
   "The Python binary to call when running auto-formatting."
   :type 'string)
 
+(defcustom elisp-autofmt-cache-packages-default
+  ;; (list "custom" "subr")
+  nil
+  "Packages to load by default (without being explicitly required)."
+  :type '(repeat string))
+
+(defcustom elisp-autofmt-ignore-autoload-packages
+  (list
+    "babel"
+    "gnus-fun"
+    "gnus-xmas"
+    "mailcrypt"
+    "mc-toplev"
+    "message"
+    "messagexmas"
+    "mh-tool-bar"
+    "nnimap"
+    "vcard")
+  "Packages to load by default (without being explicitly required)."
+  :type '(repeat string))
+
+(defcustom elisp-autofmt-cache-directory
+  (file-name-concat (locate-user-emacs-file ".cache" ".cache") "elisp-autofmt")
+  "The directory to store cache data."
+  :type 'string)
+
+
 ;; Internal variables.
 
 ;; Run this command to format.
 (defvar elisp-autofmt--bin)
 (setq elisp-autofmt--bin (file-name-sans-extension load-file-name))
 
+(defconst elisp-autofmt--this-file load-file-name)
+
+
+;; ---------------------------------------------------------------------------
+;; Internal Introspection / Cache Functions
+
+;; For `find-library-name'.
+(require 'find-func)
+;; For `file-loadhist-lookup'.
+(require 'loadhist)
+;; For `find-lisp-object-file-name'.
+;; (require 'help-fns)
+
+(defun elisp-autofmt--cache-api-val-as-str (val)
+  "Return the string representation of VAL (use for JSON encoding)."
+  (cond
+    ((symbolp val)
+      (concat "\"" (symbol-name val) "\""))
+    (t
+      (number-to-string val))))
+
+(defun elisp-autofmt--cache-api-file-is-older-list (file-test file-list)
+  "Return t when FILE-TEST is older than any files in FILE-LIST."
+  (catch 'result
+    (let ((file-test-time (file-attribute-modification-time (file-attributes file-test))))
+      (dolist (file-new file-list)
+        (when
+          (time-less-p
+            file-test-time
+            (file-attribute-modification-time (file-attributes file-new)))
+          (throw 'result t)))
+      nil)))
+
+(defun elisp-autofmt--cache-api-file-is-older (file-test &rest file-list)
+  "Return t when FILE-TEST is older than any files in FILE-LIST."
+  (elisp-autofmt--cache-api-file-is-older-list file-test file-list))
+
+(defun elisp-autofmt--cache-api-encode-name (filename)
+  "Return the cache name in cache-dir from FILENAME."
+  (concat (url-hexify-string filename) ".json"))
+
+(defun elisp-autofmt--cache-api-directory-ensure ()
+  "Ensure the cache API directory exists."
+  (unless (file-directory-p elisp-autofmt-cache-directory)
+    (make-directory elisp-autofmt-cache-directory t)))
+
+(defun elisp-autofmt--cache-api-insert-function-to-file (sym-id sym-ty arity)
+  "Insert JSON data from SYM-ID, SYM-TY and ARITY."
+  ;; `arity' is an argument because built-in functions use different logic.
+
+  (insert "\"" (string-replace "\\" "\\\\" (symbol-name sym-id)) "\": ")
+  (insert
+    "["
+    (concat "\"" (symbol-name sym-ty) "\"")
+    ", "
+    (elisp-autofmt--cache-api-val-as-str (car arity))
+    ", "
+    (elisp-autofmt--cache-api-val-as-str (cdr arity))
+    "],\n"))
+
+(defun elisp-autofmt--fn-type (sym-id)
+  "Return the type of function SYM-ID or nil."
+  (cond
+    ((functionp sym-id)
+      'func)
+    ((macrop sym-id)
+      'macro)
+    ((special-form-p sym-id)
+      'special)
+    (t
+      nil)))
+
+(defun elisp-autofmt--cache-api-generate-for-builtins (filepath)
+  "Generate API cache for built-in output at FILEPATH."
+  (with-temp-buffer
+    (insert "{\n")
+    (insert "\"functions\": {\n")
+    (let ((block-beg (point)))
+      (mapatoms
+        (lambda (sym-id)
+          (let ((sym-fn (symbol-function sym-id)))
+            (when sym-fn
+              (let
+                (
+                  (auto-load-pkg (and (autoloadp sym-fn) (cadr sym-fn)))
+                  (sym-ty (elisp-autofmt--fn-type sym-id)))
+
+                (when
+                  (and
+                    sym-ty
+                    ;; Is it non-interactive?
+                    ;; (not (commandp (symbol-function sym-id)))
+                    ;; Is it built-in? (speeds up accessing the file-path which is slow).
+                    (subrp sym-fn)
+                    (or
+                      (null auto-load-pkg)
+                      (not (member auto-load-pkg elisp-autofmt-ignore-autoload-packages))))
+                  ;; (autoload sym-id)
+
+                  ;; Note that we could check for C-source only using.
+                  ;; (find-lisp-object-file-name sym-id sym-fn)
+
+                  (when t
+                    ;; (eq file 'C-source)
+                    (elisp-autofmt--cache-api-insert-function-to-file
+                      sym-id sym-ty
+                      (cond
+                        ((subrp sym-fn)
+                          (subr-arity sym-fn))
+                        (t
+                          (func-arity sym-id)))))))))))
+      ;; Remove trailing comma (tsk).
+      (delete-region (max block-beg (- (point) 2)) (max block-beg (- (point) 1))))
+    (insert "}\n") ;; "functions".
+    (insert "}\n")
+    (write-region nil nil filepath nil 0)))
+
+(defun elisp-autofmt--cache-api-generate-for-package (filepath package-id skip-require)
+  "Generate API cache for PACKAGE-ID at FILEPATH.
+
+When SKIP-REQUIRE is non-nil, the package is not required."
+  (let ((package-sym (intern package-id)))
+    (when
+      (cond
+        (skip-require
+          t)
+        ((member package-id (list "subr"))
+          t)
+        ((with-demoted-errors "%S" (require package-sym) t)
+          t)
+        (t
+          (message "Unable to load %s" package-id)
+          nil))
+
+      ;; Ensure the cache is newer than it's source.
+      (with-temp-buffer
+        (insert "{\n")
+        ;; Allow for other kinds of data in these files in the future.
+        (insert "\"functions\": {\n")
+        (let ((block-beg (point)))
+          (let ((defs (file-loadhist-lookup package-id)))
+            (while defs
+              (let ((n (pop defs)))
+                (when (consp n)
+                  (pcase-let ((`(,_sym-ty-xx . ,sym-id) n))
+                    (let ((sym-ty (elisp-autofmt--fn-type sym-id)))
+                      (when sym-ty
+                        (elisp-autofmt--cache-api-insert-function-to-file
+                          sym-id
+                          sym-ty
+                          (func-arity sym-id))))))))
+            ;; )
+            ;; Remove trailing comma (tsk).
+            (delete-region (max block-beg (- (point) 2)) (max block-beg (- (point) 1))))
+          (insert "}\n") ;; "functions".
+          (insert "}\n")
+          (write-region nil nil filepath nil 0))))))
+
+(defun elisp-autofmt-gen-builtin-defs ()
+  "Generate builtin definitions.
+
+Writes outputs to `ELISP_AUTOFMT_OUTPUT'."
+  (elisp-autofmt--cache-api-generate-for-builtins (getenv "ELISP_AUTOFMT_OUTPUT")))
+
+(defun elisp-autofmt-gen-package-defs ()
+  "Generate builtin definitions.
+
+Uses package from environment variable `ELISP_AUTOFMT_PACKAGE'.
+Writes outputs to environment variable `ELISP_AUTOFMT_OUTPUT'."
+  (elisp-autofmt--cache-api-generate-for-package
+    (getenv "ELISP_AUTOFMT_OUTPUT")
+    (getenv "ELISP_AUTOFMT_PACKAGE")
+    nil))
+
+(defun elisp-autofmt--cache-api-ensure-cache-for-emacs (use-external-emacs)
+  "Ensure cache exists.
+
+Call an external Emacs when USE-EXTERNAL-EMACS is non-nil."
+  (let*
+    (
+      ;; Emacs binary location.
+      (filename (expand-file-name invocation-name invocation-directory))
+      (filename-cache-name-only (elisp-autofmt--cache-api-encode-name filename))
+      (filename-cache-name-full
+        (file-name-concat elisp-autofmt-cache-directory filename-cache-name-only)))
+    (when
+      (or
+        (not (file-exists-p filename-cache-name-full))
+        (elisp-autofmt--cache-api-file-is-older filename-cache-name-full filename))
+
+      (cond
+        (use-external-emacs
+          (let
+            (
+              (process-environment
+                (cons
+                  (concat "ELISP_AUTOFMT_OUTPUT=" filename-cache-name-full)
+                  process-environment)))
+            (call-process
+              filename
+              nil
+              nil
+              nil
+
+              ;; args.
+              "--batch"
+              "-l"
+              elisp-autofmt--this-file
+              "--eval"
+              "-f"
+              "elisp-autofmt-gen-builtin-defs")))
+        (t
+          (elisp-autofmt--cache-api-generate-for-builtins filename-cache-name-full))))
+    filename-cache-name-only))
+
+(defun elisp-autofmt--cache-api-ensure-cache-for-package (package-id skip-require)
+  "Ensure cache for PACKAGE-ID is up to date in CACHE-DIR.
+
+When SKIP-REQUIRE is set, don't require the package."
+  (let ((package-sym (intern package-id)))
+
+    (when
+      (cond
+        (skip-require
+          t)
+        ((with-demoted-errors "%S" (require package-sym) t)
+          t)
+        (t
+          (message "Unable to load %s" package-id)
+          nil))
+
+      (let*
+        (
+          (filename (find-library-name package-id))
+          (filename-cache-name-only (elisp-autofmt--cache-api-encode-name filename))
+          (filename-cache-name-full
+            (file-name-concat elisp-autofmt-cache-directory filename-cache-name-only)))
+
+        ;; Ensure the cache is newer than it's source.
+        (when
+          (or
+            (not (file-exists-p filename-cache-name-full))
+            (elisp-autofmt--cache-api-file-is-older filename-cache-name-full filename))
+          (elisp-autofmt--cache-api-generate-for-package
+            filename-cache-name-full
+            package-id
+            skip-require))
+        filename-cache-name-only))))
+
+(defun elisp-autofmt--cache-api-cache-update ()
+  "Ensure packages are up to date."
+  (elisp-autofmt--cache-api-directory-ensure)
+  (let ((cache-files (list)))
+    (push (elisp-autofmt--cache-api-ensure-cache-for-emacs t) cache-files)
+    (dolist (package-id elisp-autofmt-cache-packages-default)
+      (push (elisp-autofmt--cache-api-ensure-cache-for-package package-id t) cache-files))
+    cache-files))
+
 
 ;; ---------------------------------------------------------------------------
 ;; Internal Functions
-
-(defun elisp-autofmt--generate-defs (temp-file-cfg)
-  "Generate a definition list at path TEMP-FILE-CFG."
-  (with-temp-file temp-file-cfg
-    (insert
-      ;; Declare singletons.
-      "many = object()\n"
-      ;; For our purposes this is the same as many.
-      "unevalled = object()\n"
-      ;; Declare it as a variable so it can be imported.
-      "fn_arity = {\n")
-    (mapatoms
-      (lambda (x)
-        (when
-          (and
-            ;; Does x name a function?
-            (fboundp x)
-            ;; Extra check.
-            (or (functionp x) (macrop x))
-            ;; Is it non-interactive?
-            (not (commandp (symbol-function x)))
-            ;; Is it built-in?
-            ;; (symbol-function x)
-            ;; (subrp (symbol-function x))
-            )
-          ;; (message "%S %s" (fboundp x) (symbol-name x))
-          (let
-            (
-              (arity
-                (condition-case _err
-                  (func-arity x)
-                  (error nil))))
-            (when arity
-              (let
-                (
-                  (arity-min (car arity))
-                  (arity-max (cdr arity)))
-                (insert
-                  ;; Key.
-                  "r\"\"\"" (symbol-name x) "\"\"\": "
-                  ;; Value.
-                  (format "(%S, %S),\n" arity-min arity-max))))))))
-    (insert "}")))
 
 (defun elisp-autofmt--replace-buffer-contents-with-fastpath (buf)
   "Replace buffer contents with BUF, fast-path when undo is disabled.
@@ -150,10 +392,11 @@ Optional argument ASSUME-FILE-NAME overrides the file name used for this buffer.
           (t
             'utf-8)))
 
-      (temp-file-cfg
+      ;; Cache files.
+      (cache-defs
         (cond
           (elisp-autofmt-use-function-defs
-            (make-temp-file "elisp-autofmt_defs" nil ".py"))
+            (elisp-autofmt--cache-api-cache-update))
           (t
             nil)))
 
@@ -177,13 +420,13 @@ Optional argument ASSUME-FILE-NAME overrides the file name used for this buffer.
           ;; Optionally read in definitions.
           (cond
             (elisp-autofmt-use-function-defs
-              (list (format "--fmt-defs=%s" temp-file-cfg)))
+              (list
+                (concat
+                  "--fmt-defs-dir="
+                  (convert-standard-filename (expand-file-name elisp-autofmt-cache-directory)))
+                (concat "--fmt-defs=" (mapconcat 'identity cache-defs ":"))))
             (t
               (list))))))
-
-    ;; Write a configuration file.
-    (when elisp-autofmt-use-function-defs
-      (setq (elisp-autofmt--generate-defs)))
 
     (let
       (
@@ -227,11 +470,7 @@ Optional argument ASSUME-FILE-NAME overrides the file name used for this buffer.
               exit-code)
             nil)
           (t
-            (elisp-autofmt--replace-buffer-contents-with-fastpath stdout-buffer)))))
-
-    ;; Cleanup.
-    (when elisp-autofmt-use-function-defs
-      (delete-file temp-file-cfg))))
+            (elisp-autofmt--replace-buffer-contents-with-fastpath stdout-buffer)))))))
 
 (defun elisp-autofmt--region (&optional assume-file-name)
   "Auto format the current region.
@@ -253,13 +492,14 @@ Optional argument ASSUME-FILE-NAME overrides the file name used for this buffer.
   (with-current-buffer buf (elisp-autofmt--region)))
 
 (defun elisp-autofmt--buffer-format-for-save-hook ()
+  "The hook to run on buffer saving to format the buffer."
   ;; Demote errors as this is user configurable, we can't be sure it wont error.
   (when (with-demoted-errors "elisp-autofmt: Error %S" (funcall elisp-autofmt-on-save-p))
     (elisp-autofmt-buffer))
   ;; Continue to save.
   nil)
 
-(defun elisp-autofmt--enable (&optional force)
+(defun elisp-autofmt--enable ()
   "Setup an auto-format save hook for this buffer."
   ;; Buffer local hook.
   (add-hook 'before-save-hook #'elisp-autofmt--buffer-format-for-save-hook nil t))
@@ -278,7 +518,7 @@ Optional argument ASSUME-FILE-NAME overrides the file name used for this buffer.
   (elisp-autofmt--buffer-impl (current-buffer)))
 
 ;;;###autoload
-(defun elisp-autopep8-check-elisp-autofmt-exists ()
+(defun elisp-autofmt-check-elisp-autofmt-exists ()
   "Return non-nil when `.elisp-autofmt' is found in a parent directory."
   (let ((cfg (locate-dominating-file (file-name-directory buffer-file-name) ".elisp-autofmt")))
     (not (null cfg))))
---
+++ tests/generate_defs.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+# GPL License, Version 3.0 or later
+
+import shutil
+import subprocess
+import unittest
+import tempfile
+
+import os
+
+EMACS_BIN = "emacs"
+VERBOSE = os.environ.get('VERBOSE', False)
+# TEMP_LOCAL = '/tmp/out'
+
+BASE_DIR = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+
+USTOW_DIR = '_ustow'
+
+
+def generate_defs_builtin(output):
+    cmd = (
+        EMACS_BIN,
+        "--batch",
+        "-l", os.path.join(BASE_DIR, "elisp-autofmt.el"),
+        "-f", "elisp-autofmt-gen-builtin-defs",
+    )
+    env = os.environ.copy()
+    env.update({
+        "ELISP_AUTOFMT_OUTPUT": output,
+    })
+    subprocess.check_call(cmd, env=env)
+
+
+def generate_defs_package(output, package):
+    cmd = (
+        EMACS_BIN,
+        "--batch",
+        "-l", os.path.join(BASE_DIR, "elisp-autofmt.el"),
+        "-f", "elisp-autofmt-gen-package-defs",
+    )
+    env = os.environ.copy()
+    env.update({
+        "ELISP_AUTOFMT_OUTPUT": output,
+        "ELISP_AUTOFMT_PACKAGE": package,
+    })
+    subprocess.check_call(cmd, env=env)
+
+
+def generate_defs_package_as_json(package):
+    import json
+    with tempfile.TemporaryDirectory() as d:
+        p = os.path.join(d, "out.json")
+        generate_defs_package(p, package)
+        with open(p, 'r', encoding='utf-8') as fh:
+            return json.load(fh)
+
+
+def generate_defs_builtin_as_json():
+    import json
+    with tempfile.TemporaryDirectory() as d:
+        p = os.path.join(d, "out.json")
+        generate_defs_builtin(p)
+        with open(p, 'r', encoding='utf-8') as fh:
+            return json.load(fh)
+
+
+class MyTestCase(unittest.TestCase):
+
+    pass
+
+    # def assertFileExists(self, filepath, msg=None):
+    #     if not os.path.exists(filepath):
+    #         if msg is None:
+    #             msg = 'File %r does not exist' % os.path.relpath(filepath, TEMP_LOCAL)
+    #         raise self.failureException(msg)
+
+    def setUp(self):
+        pass
+
+        # # for running single tests
+        # if __name__ != '__main__':
+        #     self._data = global_setup()
+
+        # if not os.path.isdir(TEMP_LOCAL):
+        #     os.makedirs(TEMP_LOCAL)
+
+    def tearDown(self):
+        pass
+
+        # shutil.rmtree(TEMP_LOCAL)
+
+        # # for running single tests
+        # if __name__ != '__main__':
+        #     global_teardown(self._data)
+
+
+class SimpleTestBuiltinPackage_SubrX(MyTestCase):
+    def test_check_simple(self):
+        data = generate_defs_package_as_json("subr-x")
+        self.assertEqual(data['functions']['string-join'], ['func', 1, 2])
+        self.assertEqual(data['functions']['named-let'], ['macro', 2, 'many'])
+
+
+class SimpleTestBuiltinPackage_Subr(MyTestCase):
+    def test_check_simple(self):
+        data = generate_defs_package_as_json("subr")
+        self.assertEqual(data['functions']['with-syntax-table'], ['macro', 1, 'many'])
+        self.assertEqual(data['functions']['defvar-local'], ['macro', 2, 3])
+
+
+class SimpleTestBuiltinPackage_Simple(MyTestCase):
+    def test_check_simple(self):
+        data = generate_defs_package_as_json("simple")
+        self.assertEqual(data['functions']['backward-word'], ['func', 0, 1])
+        self.assertEqual(data['functions']['shell-command-on-region'], ['func', 3, 8])
+
+
+class SimpleTestBuiltinPackage_File(MyTestCase):
+    def test_check_simple(self):
+        data = generate_defs_package_as_json("files")
+        self.assertEqual(data['functions']['directory-abbrev-make-regexp'], ['func', 1, 1])
+        self.assertEqual(data['functions']['insert-directory-safely'], ['func', 2, 4])
+
+
+class SimpleTestBuiltin(MyTestCase):
+    def test_check_simple(self):
+        data = generate_defs_builtin_as_json()
+        self.assertEqual(data['functions']['file-attributes'], ['func', 1, 2])
+        self.assertEqual(data['functions']['isearch-forward'], ['func', 0, 2])
+
+
+def global_setup():
+    data = None
+    # shutil.rmtree(TEMP_LOCAL, ignore_errors=True)
+    return data
+
+
+def global_teardown(_data):
+    # shutil.rmtree(TEMP_LOCAL, ignore_errors=True)
+    pass
+
+
+if __name__ == '__main__':
+    data = global_setup()
+    unittest.main(exit=False)
+    global_teardown(data)
